72b3e1e8b85899ad774d765bd62a86b97ba68e79
Karen Rustad <karen.rustad@gmail.com>
2011-06-29 16:53:32 -0700

    made new rails app

---
+++ code-jockey/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeJockey::Application.config.secret_token = '32409e12e080ebf22b305375e869f9272ff5e6a6a70857bfb3a4a6534f814646399989f483f1e42de62c655b1c206a1a223ef8e811e8d1bcaf39bde1d848a0b3'
